d31b83bf2385d530abe827ddac25be1326a73657
Alwaleed Ahmed <alwleedamado@gmail.com>
2025-10-22 07:37:46 +0200

    chore(cleanup) systax formating and creating unit test

---
+++ Src/TSCompilerFrontend/Parser/Factory.cs
@@ -0,0 +1,15 @@
+﻿using TSCompilerFrontend.TsTypes;
+
+namespace TSCompilerFrontend.Parser;
+
+public static class Factory
+{
+  public static INode SkipPartiallyEmittedExpressions(INode node)
+  {
+    while (node.Kind == SyntaxKind.PartiallyEmittedExpression)
+      node = ((PartiallyEmittedExpression)node).Expression;
+
+
+    return node;
+  }
+}
---
+++ Tests/TSCompilerFrontend.UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Security;
+
+namespace TSCompilerFrontend.UnitTest;
+
+public class Tests
+{
+  private ITypeScriptAST _ast;
+  [SetUp]
+  public void Setup()
+  {
+    _ast = new TypeScriptAST(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "\\app.component.ts"));
+  }
+
+  [Test]
+  public void ShouldCreateValidAst()
+  {
+    var dec = _ast.GetDescendants();
+    Assert.That(dec.Count(), Is.GreaterThan(0));
+  }
+}
